ba7591c6364cecf6803a227ab9757a14ae9657ae
YanethZG <jeanethzelaya56@gmail.com>
2022-06-12 23:28:05 -0600

    Agregando funcion generar reportes

--- app/src/main/java/com/jonasgarcia/sys_gestion_incidentes/MainActivity.java
+++ app/src/main/java/com/jonasgarcia/sys_gestion_incidentes/MainActivity.java
@@ -2,19 +2,48 @@ package com.jonasgarcia.sys_gestion_incidentes;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.jonasgarcia.sys_gestion_incidentes.employeeIU.HomeFragment;
 import com.jonasgarcia.sys_gestion_incidentes.employeeIU.ProfileFragment;
+import com.jonasgarcia.sys_gestion_incidentes.models.Usuario;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    static List<Usuario> ListaUsuarios = new ArrayList<>();
+    Button btnCrearPDF;
+
 
     //    SharedPreferences
     SharedPreferences preferences;
@@ -27,6 +56,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkPermission()) {
+            Toast.makeText(this, "Permiso Aceptado", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
+
+        btnCrearPDF = findViewById(R.id.btnCrearPdf);
+
+        ListaUsuarios.add(new Usuario("karla", "yaneth", "karla85@gmail.com"));
+        ListaUsuarios.add(new Usuario("karla", "yaneth", "karla85@gmail.com"));
+        ListaUsuarios.add(new Usuario("karla", "yaneth", "karla85@gmail.com"));
+
+        btnCrearPDF.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                CrearPDF();
+            }
+        });
+
 
         preferences = getSharedPreferences("session", MODE_PRIVATE);
 
@@ -51,6 +100,66 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    public static void CrearPDF(){
+        try {
+            String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/EjemploITextPDF";
+            File dir = new File(path);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            File file = new File(dir, "usuarios.pdf");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            Document documento = new Document();
+            PdfWriter.getInstance(documento, fileOutputStream);
+
+            documento.open();
+            Paragraph titulo = new Paragraph(
+                    "Lista de ususarios \n\n\n",
+                    FontFactory.getFont("arial", 22, Font.BOLD, BaseColor.BLUE)
+            );
+
+            documento.add(titulo);
+
+            PdfPTable tabla = new PdfPTable(3);
+            tabla.addCell("USUARIO");
+            tabla.addCell("NOMBRE");
+            tabla.addCell("CORREO");
+
+            for (int i = 0 ; i < ListaUsuarios.size() ; i++) {
+                tabla.addCell(ListaUsuarios.get(i).getNombre());
+                tabla.addCell(ListaUsuarios.get(i).getApellido());
+                tabla.addCell(ListaUsuarios.get(i).getEmail());
+            }
+
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkPermission() {
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResult){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResult);
+        if (requestCode == 200){
+            if (grantResult.length > 0){
+                boolean writeStorage = grantResult[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResult[1] == PackageManager.PERMISSION_GRANTED;
+            }
+        }
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 200);
+    }
     public void loadFragment(Fragment fragment){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_container, fragment);
---
+++ app/src/main/java/com/jonasgarcia/sys_gestion_incidentes/models/Usuario.java
@@ -0,0 +1,35 @@
+package com.jonasgarcia.sys_gestion_incidentes.models;
+
+public class Usuario{
+    String nombre, apellido, email;
+
+    public Usuario(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
