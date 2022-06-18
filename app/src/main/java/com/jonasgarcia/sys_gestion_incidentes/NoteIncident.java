d9d344fb02107d9fac945bf7c00c649e7f2aaedf
josuecorea5 <josuepaiz487@gmail.com>
2022-06-18 01:25:26 -0600

    Add select incident

--- app/src/main/java/com/jonasgarcia/sys_gestion_incidentes/NoteIncident.java
+++ app/src/main/java/com/jonasgarcia/sys_gestion_incidentes/NoteIncident.java
@@ -1,20 +1,16 @@
-package com.jonasgarcia.sys_gestion_incidentes.adminUI;
+package com.jonasgarcia.sys_gestion_incidentes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,11 +22,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.jonasgarcia.sys_gestion_incidentes.NoteIncident;
-import com.jonasgarcia.sys_gestion_incidentes.R;
 import com.jonasgarcia.sys_gestion_incidentes.config.Configuration;
-import com.jonasgarcia.sys_gestion_incidentes.utils.DialogLoading;
-import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -39,89 +31,71 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DetailIncident extends AppCompatActivity {
+public class NoteIncident extends AppCompatActivity {
+    LinearLayout btnAddNoteIncident_Back;
+    EditText addNote;
+    TextView errorNote;
+    Configuration config = new Configuration();
+    String URL = config.urlIncidentes;
     SharedPreferences preferences;
-    int id_incident = 0;
-    Configuration configuration = new Configuration();
-    LinearLayout btnDetailsIncident_Back;
-    String URL = configuration.urlIncidentes;
-    String basePath = configuration.urlBase;
-    ImageView imgIncident;
-    String state = "";
-    TextView txtIncident, txtDescription,txtState;
-    DialogLoading dialogLoading;
-    Button btnEdit, btnDelete;
-
+    Button btnAddNote;
+    String id_incident = "0", token = "", id_rol = "0", state = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_incident);
         preferences = this.getSharedPreferences("session", Context.MODE_PRIVATE);
-        dialogLoading = new DialogLoading(DetailIncident.this);
-        setContentView(R.layout.activity_detail_incident);
-        btnDetailsIncident_Back = findViewById(R.id.btnDetailsIncident_Back);
-        imgIncident = findViewById(R.id.imgIncident);
-        txtIncident = findViewById(R.id.txtIncident);
-        txtDescription = findViewById(R.id.txtDescription);
-        txtState = findViewById(R.id.txtState);
-        btnEdit = findViewById(R.id.btnIncidentEdit);
-        btnDelete = findViewById(R.id.btnDeleteIncident);
 
-        btnDetailsIncident_Back.setOnClickListener(new View.OnClickListener() {
+        btnAddNoteIncident_Back = findViewById(R.id.btnAddNoteIncident_Back);
+        addNote = findViewById(R.id.addNote);
+        errorNote = findViewById(R.id.errorNote);
+        btnAddNote = findViewById(R.id.btnAddNote);
+
+        btnAddNoteIncident_Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
 
-        btnEdit.setOnClickListener(new View.OnClickListener() {
+        btnAddNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(DetailIncident.this, NoteIncident.class);
-                intent.putExtra("id_incident",Integer.toString(id_incident));
-                intent.putExtra("estado", state);
-                startActivity(intent);
+                addNoteIncident();
             }
         });
 
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onPostResume() {
+        super.onPostResume();
         Bundle extras = getIntent().getExtras();
-        dialogLoading.startDialog();
-        if(extras != null){
-            id_incident = extras.getInt("id_incidente");
-            state = extras.getString("estado");
+        if (extras != null) {
+            id_incident = extras.getString("id_incident");
+            if(extras.getString("estado").equals("0")) {
+                state = "1";
+            }else {
+                state = "0";
+            }
+            token = preferences.getString("token", "");
         }
-
-        getIncidentById();
     }
 
-    public void getIncidentById(){
-        RequestQueue requestQueue = Volley.newRequestQueue(DetailIncident.this);
+    public void addNoteIncident() {
+        RequestQueue requestQueue = Volley.newRequestQueue(NoteIncident.this);
         StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                Toast.makeText(NoteIncident.this,"AAAAA",
+                        Toast.LENGTH_LONG).show();
                 try {
                     JSONObject result = new JSONObject(response);
-                    JSONObject data = result.getJSONObject("result");
-
-                    txtIncident.setText(data.getString("tipo"));
-                    txtDescription.setText(data.getString("descripcion"));
-                    String urlImg = basePath + data.getString("imagen");
-                    imgIncident.setImageURI(Uri.parse(urlImg));
-                    Picasso.get().load(urlImg).into(imgIncident);
-                    if(data.getString("estado").equals("0")) {
-                        txtState.setText("Sin resolver");
-                    }else {
-                        txtState.setText("Resuelto");
-                    }
-                    dialogLoading.dismissDialog();
+                    showToastOK("Nota agregada!.");
+                    finish();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    dialogLoading.dismissDialog();
                 }
             }
         }, new Response.ErrorListener() {
@@ -137,23 +111,57 @@ public class DetailIncident extends AppCompatActivity {
                 try {
                     body = new String(error.networkResponse.data, "UTF-8");
                     JSONObject data = new JSONObject(body);
-                    showToastErr(data.getString("msg"));
+                    JSONObject errors = data.getJSONObject("validationError");
+
+                    if (!errors.isNull("incidente")) {
+                        setBackgroundError(addNote);
+                        errorNote.setText(errors.getString("incidente"));
+                    }
+
                 } catch (UnsupportedEncodingException | JSONException e) {
+
                     showToastErr(e.getMessage());
+                    Toast.makeText(NoteIncident.this,token,
+                            Toast.LENGTH_LONG).show();
                 }
-                dialogLoading.dismissDialog();
+                Toast.makeText(NoteIncident.this,addNote.getText().toString() + " " +
+                        id_incident + " " + state,
+                        Toast.LENGTH_LONG).show();
             }
         }) {
             @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", token);
+                return headers;
+            }
+
+            @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<>();
-                params.put("action", "get_incident");
-                params.put("id", Integer.toString(id_incident));
+                params.put("action", "update_admin");
+                params.put("id_incidente", id_incident);
+                params.put("estado", state);
+                params.put("nota", addNote.getText().toString());
+
                 return params;
             }
         };
         requestQueue.add(request);
     }
+    public void showToastOK(String msg) {
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.custom_toast_ok, (ViewGroup) findViewById(R.id.ll_custom_toast_ok));
+        TextView tvMessage = view.findViewById(R.id.tvMsg);
+        tvMessage.setText(msg);
+
+        Toast toastOK = new Toast(getApplicationContext());
+        toastOK.setGravity(Gravity.BOTTOM, 0, 300);
+        toastOK.setDuration(Toast.LENGTH_LONG);
+        toastOK.setView(view);
+        toastOK.show();
+    }
+
     public void showToastErr(String msg) {
         LayoutInflater inflater = getLayoutInflater();
         View view = inflater.inflate(R.layout.custom_toast_err, (ViewGroup) findViewById(R.id.ll_custom_toast_err));
@@ -165,5 +173,9 @@ public class DetailIncident extends AppCompatActivity {
         toastOK.setDuration(Toast.LENGTH_LONG);
         toastOK.setView(view);
         toastOK.show();
+    }
+    public void setBackgroundError(EditText editText){
+        editText.setBackgroundResource(R.drawable.input_rounded_error);
+        editText.setHintTextColor(getColor(R.color.slate_50));
     }
 }
